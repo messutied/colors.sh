7327906732bead145d13b38cda4422761819606a
Eduardo Messuti <messuti.edd@gmail.com>
2018-02-06 12:45:40 +0100

    added footer

--- src/containers/RootContainer/RootContainer.js
+++ src/containers/RootContainer/RootContainer.js
@@ -8,6 +8,21 @@ export default class RootContainer extends React.Component {
       <div>
         <span className="logo">🌈💻 Colors.sh</span>
         <HomeScreen />
+        <footer>
+          <a
+            href="https://misc.flogisoft.com/bash/tip_colors_and_formatting"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Full docs
+          </a>
+          <div className="author">
+            Made by
+            <a href="https://twitter.com/messutied" target="_blank" rel="noopener noreferrer">
+              @messutied
+            </a>
+          </div>
+        </footer>
       </div>
     );
   }
