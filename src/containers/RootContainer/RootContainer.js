90d1845b1ee952559c5ad75ae7ae183ef50f170c
Eduardo Messuti <messuti.edd@gmail.com>
2018-04-02 11:18:29 +0200

    removing fork-me ribbon on mobile + added stars & follow buttons

--- src/containers/RootContainer/RootContainer.js
+++ src/containers/RootContainer/RootContainer.js
@@ -9,18 +9,38 @@ export default class RootContainer extends React.Component {
         <span className="logo">🌈💻 Colors.sh</span>
         <HomeScreen />
         <footer>
-          <a
-            href="https://misc.flogisoft.com/bash/tip_colors_and_formatting"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Source docs
-          </a>
-          <div className="author">
-            Made by&nbsp;
-            <a href="https://twitter.com/messutied" target="_blank" rel="noopener noreferrer">
-              @messutied
+          <div className="github-buttons">
+            <iframe
+              src="https://ghbtns.com/github-btn.html?user=messutied&repo=colors.sh&type=star&count=true"
+              frameBorder="0"
+              scrolling="0"
+              width="90px"
+              height="20px"
+              title="Github stars"
+            />
+            <iframe
+              src="https://ghbtns.com/github-btn.html?user=twbs&repo=bootstrap&type=fork&count=false"
+              frameBorder="0"
+              scrolling="0"
+              width="60px"
+              height="20px"
+              title="Github stars"
+            />
+          </div>
+          <div className="footer-info">
+            <a
+              href="https://misc.flogisoft.com/bash/tip_colors_and_formatting"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Source docs
             </a>
+            <div className="author">
+              Made by&nbsp;
+              <a href="https://twitter.com/messutied" target="_blank" rel="noopener noreferrer">
+                @messutied
+              </a>
+            </div>
           </div>
         </footer>
       </div>
